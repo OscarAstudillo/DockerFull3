ba354619eb7bdfc873f7ebced30be779047b0678
Oscar Astudillo <osca.astudillo@duocuc.cl>
2026-03-12 19:44:46 -0300

    segundo commit

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,22 +1,33 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Producto;
+import com.example.demo.repository.ProductoRepository;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
 
+    @Autowired
+    private ProductoRepository productoRepository;
+
     @GetMapping
-    public List<Producto> getAllProductos() {
-        Producto producto = new Producto();
-        producto.setId(1L);
-        producto.setNombre("Producto de ejemplo");
-        return List.of(producto);
+    public List<Producto> getProductos() {
+        return productoRepository.findAll();
+    }
+
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoRepository.save(producto);
     }
     
 }
